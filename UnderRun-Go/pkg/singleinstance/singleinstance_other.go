516fcf1d64ed520c6c1334162b8a8fa926123b05
tianyi <i@terry.ee>
2026-01-19 12:23:17 +0800

    update

--- UnderRun-Go/pkg/singleinstance/singleinstance_other.go
+++
@@ -1,8 +0,0 @@
-//go:build !windows
-
-package singleinstance
-
-// CheckAndActivate is a no-op on non-Windows systems for now
-func CheckAndActivate(mutexName string, windowTitle string) bool {
-	return false
-}
